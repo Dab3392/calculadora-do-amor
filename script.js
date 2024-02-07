edfdf7bcdedb0d38983c63aa7546459daeee4120
Dab3392 <142180417+Dab3392@users.noreply.github.com>
2024-02-07 14:54:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelector("form").addEventListener("submit", (e) => {
     if (n < 1 || n > 50 || isNaN(n)) return "Você é Burro? O Numero Deve Ser De 0 A 50!";
     if (n <= 10) return "A Sua Namorada Vai Ficar Brava se Ficar Sabendo Disso!";
     if (n <= 20) return "Isso é muito pouco!";
-    if (n <= 30) return "Eu Acho Que é Mais! ";
+    if (n <= 30) return "Eu  Que é Mais! ";
     if (n <= 40) return "Pode Melhorar né?";
     return "Que FoFo, Mas Claramente Sua Namorada Te Ama Mais!";
   }
