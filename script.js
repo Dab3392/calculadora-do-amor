86f2e507f3045f0074e2b76f7f0d683dede9cf4f
Dab3392 <142180417+Dab3392@users.noreply.github.com>
2024-02-07 15:04:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.querySelector("form").addEventListener("submit", (e) => {
 
   function obterMensagem(n) {
     if (n < 1 || n > 50 || isNaN(n)) return "Você é Burro? O Numero Deve Ser De 0 A 50!";
-    if (n <= 10) return "A Sua Namorada Vai Ficar Brava se Ficar Sabendo Disso!";
-    if (n <= 20) return "Isso é muito pouco!";
-    if (n <= 30) return "Eu  Que é Mais! ";
-    if (n <= 40) return "Pode Melhorar né?";
-    return "Que FoFo, Mas Claramente Sua Namorada Te Ama Mais!";
+    if (n <= 10) return "A seu dabinho Vai chorar se Ficar Sabendo Disso!";
+    if (n <= 20) return "acabou o amor?!";
+    if (n <= 30) return "vish amo mais em vai deixar?! ";
+    if (n <= 40) return "QUEEEEE SÒ ISSO??? já tem outro né :(";
+    return "Que FoFo, Mas Claramente eu te amo Mais!";
   }
 
   let timeout 
